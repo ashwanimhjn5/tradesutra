c07d5412348d7c88341403bcbecbaf03de728b31
Ashwani Mahajan <ashwani.m5@gmail.com>
2025-01-08 20:42:37 +0530

    Fixed Werkzeug compatibility issue

--- stockapp.py
+++ stockapp.py
@@ -8,6 +8,10 @@ from flask import send_file
 import matplotlib
 matplotlib.use('Agg')  # Use a non-GUI backend
 import matplotlib.pyplot as plt
+from urllib.parse import quote as url_quote
+from werkzeug.urls import url_quote
+
+
 
 
 app = Flask(__name__)
